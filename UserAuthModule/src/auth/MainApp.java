aa4f38fd31c210828b19d3708dbdf91b6cfb61d6
LunalejaS <88463714+LunalejaS@users.noreply.github.com>
2025-06-08 09:59:07 -0500

    MainApp.java

--- UserAuthModule/src/auth/MainApp.java
+++ UserAuthModule/src/auth/MainApp.java
@@ -13,22 +13,28 @@ public class MainApp {
     }
     
     // Method to test the ToDoApp functionality
-    public static void testToDoApp() {
+    //public static void testToDoApp() {
         // Create a fictitious user
-        User testUser = new User("testuser", "testpass");
+        //User testUser = new User("testuser", "testpass");
 
         // Launch the simple ToDoApp for this user
-        ToDoApp todoApp = new ToDoApp(testUser);
-        todoApp.start();    	
-    }
+        //ToDoApp todoApp = new ToDoApp(testUser);
+        //todoApp.start();    	
+    //}
     
     // Method to test the ToDoApp functionality
-    public static void testBalotoApp() {
+    //public static void testBalotoApp() {
         // Create a fictitious user
-        User testUser = new User("testuser", "testpass");
+        //User testUser = new User("testuser", "testpass");
 
         // Launch the simple ToDoApp for this user
-        BalotoApp balotoApp = new BalotoApp(testUser);
-        balotoApp.start();    	
-    }    
+        //BalotoApp balotoApp = new BalotoApp(testUser);
+        //balotoApp.start();    	
+    //}  
+
+	// Metdodo para comprobar la funcionalidad de SocialCalendarApp
+	public static void testSocialCalendarApp(){
+		User testUser = new User("testuser", "testpass");
+		socialCalendarApp = new SocialCalendarApp(testUser);
+		socialCalendarApp.start();
 }
